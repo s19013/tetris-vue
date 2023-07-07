cd137dbd333d0e60b4d6c5da3739eea4cbb80d71
知念英弥 <s19013@std.it-college.ac.jp>
2023-07-07 18:15:26 +0900

    計算ファイルから描画用の配列を出力するとこまで書いてみた

--- src/Tetris.js
+++ src/Tetris.js
@@ -1,5 +1,43 @@
-// 
+import Block from "./Block"
 
-class Tetris {
+export default class Tetris {
+    Field = []
+
+    /** 動かせるブロックたちの座標 */
+    tetrimino = {
+
+    }
+
+    constructor(){
+        // 10*10のリストにblockクラスを入れる
+        // i:縦
+        // n:横
+
+        for (let i = 0; i < 10; i++) {
+            let line = [] //横一列の配列
+            for (let n = 0; n < 10; n++) {
+                line.push(new Block())
+            }
+            this.Field.push(line)
+            line = []
+        }
+    }
+
+    /** 描写用の配列を返す */
+    display(){
+        // i:縦
+        // n:横
+        
+        let temp = []
+        for (let i = 0; i < 10; i++) {
+            let line = []
+            for (let n = 0; n < 10; n++) {
+                line.push(Number(this.Field[i][n].isFill))
+            }
+            temp.push(line)
+            line = []
+        }
+        return temp
+    }
 
 }
