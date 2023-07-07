8af8a8e191c171398a397d4398008619b343e799
知念英弥 <s19013@std.it-college.ac.jp>
2023-07-07 18:36:07 +0900

    とりあえず試しに色を塗ってみる

--- src/Tetris.js
+++ src/Tetris.js
@@ -3,16 +3,21 @@ import Block from "./Block"
 export default class Tetris {
     Field = []
 
-    /** 動かせるブロックたちの座標 */
+    /** 動かせるブロックたちの座標 と ブロックの種類*/
     tetrimino = {
-
+        type:"T",
+        Coordinate:[
+            {x:0,y:1},
+            {x:1,y:1},
+            {x:1,y:0},
+            {x:2,y:1},
+        ]
     }
 
     constructor(){
         // 10*10のリストにblockクラスを入れる
         // i:縦
         // n:横
-
         for (let i = 0; i < 10; i++) {
             let line = [] //横一列の配列
             for (let n = 0; n < 10; n++) {
@@ -21,13 +26,18 @@ export default class Tetris {
             this.Field.push(line)
             line = []
         }
+
+        // 適当に色を塗ってみる
+        for (var item of this.tetrimino.Coordinate) {
+            this.Field[item.y][item.x].isFill = true
+        }
     }
 
     /** 描写用の配列を返す */
     display(){
         // i:縦
         // n:横
-        
+
         let temp = []
         for (let i = 0; i < 10; i++) {
             let line = []
