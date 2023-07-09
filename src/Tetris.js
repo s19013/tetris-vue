c4dad06dcfa648b68919e0b7e8317c9ca8a4fc21
知念英弥 <s19013@std.it-college.ac.jp>
2023-07-09 10:19:49 +0900

    揃った列を消す処理､消した後の整地処理

--- src/Tetris.js
+++ src/Tetris.js
@@ -8,6 +8,7 @@ export default class Tetris {
 
     Field = []
     autoDropIntervalId = null
+    score = 0
 
     /** 放置してると勝手にブロックが落ちる感覚 */
     autoDropInterval = 1000 //ms
@@ -21,11 +22,17 @@ export default class Tetris {
     tetrimino = {
         type:"T",
         Coordinate:[
-            {x:4,y:0},
+            {x:3,y:1},
             {x:4,y:1},
-            {x:5,y:0},
+            {x:4,y:0},
             {x:5,y:1},
         ]
+
+        // o
+        // {x:4,y:1},
+        // {x:4,y:0},
+        // {x:5,y:1},
+        // {x:5,y:0},
 
         // T
         // {x:3,y:1},
@@ -57,38 +64,33 @@ export default class Tetris {
         this.Field[8][0].isFill = true
         this.Field[8][9].isFill = true
 
+
         // .bind(this)でthis.が使えるようになる
-        // this.autoDropIntervalId = setInterval(this.autoDrop.bind(this), this.autoDropInterval);
+        this.autoDropIntervalId = setInterval(this.autoDrop.bind(this), this.autoDropInterval);
+        
 
     }
 
     /** ボタン押された時の処理たち */
     keyDownUp(){
-        console.log("up");
+        // console.log("up");
     }
 
     keyDownDown(){
-        console.log("down");
+        // console.log("down");
         /** 動かせるかどうか確認*/
         if (this.checkCanMove.down({
                 Field:this.Field,
                 tetrimino:this.tetrimino
             })
         ) {
-            /** 今の位置を古い情報として保存 */
-            this.oldTetrimino = JSON.parse(JSON.stringify(this.tetrimino));
-
-            /** 位置を更新 */
-            for (let block of this.tetrimino.Coordinate) {
-                block.y += 1
-            }
-
-            this.moveTetrimino()
+            // autoDropとやること一緒だからautoDrop関数使う
+            this.autoDrop()
         }
     }
 
     keyDownLeft(){
-        console.log("left");
+        // console.log("left");
         /** 動かせるかどうか確認*/
         if (this.checkCanMove.left({
                 Field:this.Field,
@@ -108,7 +110,7 @@ export default class Tetris {
     }
 
     keyDownRight(){
-        console.log("right");
+        // console.log("right");
         if (this.checkCanMove.right({
                 Field:this.Field,
                 tetrimino:this.tetrimino
@@ -126,15 +128,15 @@ export default class Tetris {
     }
 
     keyDownSpace(){
-        console.log("space");
+        // console.log("space");
     }
 
     autoDrop(){
-        console.log("drop");
         /** 動かせないなら固定化する */
+        // このロジックmoveTetriminoに分けたほうがよい?
         if (this.checkCanMove.down({
-            Field:this.Field,
-            tetrimino:this.tetrimino
+                Field:this.Field,
+                tetrimino:this.tetrimino
             })
             == false
         ) {
@@ -175,12 +177,78 @@ export default class Tetris {
         this.tetrimino = {
             type:"T",
             Coordinate:[
-                {x:4,y:0},
+                {x:3,y:1},
                 {x:4,y:1},
-                {x:5,y:0},
+                {x:4,y:0},
                 {x:5,y:1},
             ]
         }
+
+        this.isColumnsAligned()
+    }
+
+    /** 列が揃っているか */
+    isColumnsAligned(){
+        /** 列の合計が
+         * 0 :空行
+         * 10:1列揃っている
+         */
+
+        /** 揃った列番号 */
+        let alignedRow = []
+        
+        /** 計算しやすいようにisFillを数値化する  */
+        let table = []
+        for (let i = 0; i < 10; i++) {
+            let line = []
+            for (let n = 0; n < 10; n++) {
+                line.push(Number(this.Field[i][n].isFill))
+            }
+            table.push(line)
+            line = []
+        }
+
+        for (let line of table) {
+            /** 配列の中身を全部足す */
+            let sum =  line.reduce(function(a,b){return a + b;});
+
+            // index番号を揃った列配列に入れる
+            if (sum == 10) {alignedRow.push(table.indexOf(line))}
+        }
+
+        console.log("alignedRow: " + JSON.stringify(alignedRow));
+
+        /** 揃った列をスコアとして足す */
+        this.score += alignedRow.length
+        /** 揃った列を消す */
+        for (let lineIndex of alignedRow) {
+            for (let block of this.Field[lineIndex]) { block.isFill = false}
+        }
+
+        /** フィールドを整える */
+        this.FieldLeveling(alignedRow)
+    }
+
+    /** 列を消したあとの処理 */
+    FieldLeveling(alignedRow){
+        // 消した列より上の列すべて一段下げたい
+
+        /** 揃った列自体を消してしまう */
+        for (let lineIndex of alignedRow){ this.Field.splice(lineIndex, 1); }
+
+        // 演出のために少しだけ待つ
+        // sleep処理
+
+        /** 消した分だけ上に加える */
+        for (let i = 0; i < alignedRow.length; i++) {
+            let line = [] //横一列の配列
+            for (let n = 0; n < 10; n++) {
+                line.push(new Block())
+            }
+            this.Field.unshift(line)
+            line = []
+        }
+
     }
 
 
