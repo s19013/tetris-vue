d7d0ac1e5b00c38171168554752b45ee219de906
知念英弥 <s19013@std.it-college.ac.jp>
2023-07-14 22:53:16 +0900

    どうしても最初の部分でエラーを起こしてしまうので初期出現位置をずらした

--- src/Tetrimino.js
+++ src/Tetrimino.js
@@ -22,10 +22,10 @@ export default class Tetrimino{
     I = {
         type:"I",
         Coordinate:[
-            {x:3,y:1},
-            {x:4,y:1},
-            {x:5,y:1},
-            {x:6,y:1},
+            {x:3,y:2},
+            {x:4,y:2},
+            {x:5,y:2},
+            {x:6,y:2},
         ],
         clockwiseAxis:2,
         counterClockwiseAxis:1
