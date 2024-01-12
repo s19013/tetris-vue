bc01be8af3efbfd53d336993e4ef5d4a3e1a90cc
知念英弥 <s19013@std.it-college.ac.jp>
2024-01-12 21:52:13 +0900

    他多数の機能を加えて1つのクラスとして機能するようにした

--- src/Next.js
+++ src/Next.js
@@ -1,26 +1,47 @@
-import Imino from "./Tetrimino/Imino";
-import Tmino from "./Tetrimino/Tmino";
-import Omino from "./Tetrimino/Omino";
-import Smino from "./Tetrimino/Smino";
-import Zmino from "./Tetrimino/Zmino";
-import Lmino from "./Tetrimino/Lmino";
-import Jmino from "./Tetrimino/Jmino";
+import Imino from "./Tetrimino/Imino.js";
+import Tmino from "./Tetrimino/Tmino.js";
+import Omino from "./Tetrimino/Omino.js";
+import Smino from "./Tetrimino/Smino.js";
+import Zmino from "./Tetrimino/Zmino.js";
+import Lmino from "./Tetrimino/Lmino.js";
+import Jmino from "./Tetrimino/Jmino.js";
 
-/** シャッフルライブラリが無いので自作するしかない */
-// aiがFisher-Yatesシャッフルアルゴリズムが一番効率的だから使えっていうからそうしてみる
-function shuffle(array) {
-    // 配列の最後の要素から順に、現在の要素とそれより前の位置にあるランダムな位置の要素を交換。
-    for (let i = array.length - 1; i > 0; i--) {
-        // 自分より前の要素を適当に選ぶ
-        const j = Math.floor(Math.random() * (i + 1));
-        // 交換
-        [array[i], array[j]] = [array[j], array[i]];
+export default class Next{
+    constructor(){
+        // あらかじめ2週分用意しとく
+        this.list = [...this.getShuffledArray(),...this.getShuffledArray()]
     }
-    return array;
-}
 
-// 1セットを返す
-export function passSet(){
-    let base = [new Imino(),new Tmino(),new Omino(),new Smino(),new Zmino(),new Lmino(),new Jmino]
-    return shuffle(base) 
+    // initialize(){}
+
+    /** シャッフルライブラリが無いので自作するしかない */
+    // aiがFisher-Yatesシャッフルアルゴリズムが一番効率的だから使えっていうからそうしてみる
+    shuffle(array) {
+        // 配列の最後の要素から順に、現在の要素とそれより前の位置にあるランダムな位置の要素を交換。
+        for (let i = array.length - 1; i > 0; i--) {
+            // 自分より前の要素を適当に選ぶ
+            const j = Math.floor(Math.random() * (i + 1));
+            // 交換
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    // シャッフルした1巡分を返す
+    getShuffledArray(){
+        let base = [new Imino(),new Tmino(),new Omino(),new Smino(),new Zmino(),new Lmino(),new Jmino]
+        return this.shuffle(base) 
+    }
+
+    // 次落とすテトリミノを渡す
+    getNextTetrimino(){
+        return this.list.shift()
+    }
+
+    /** nextを補充するかどうか */
+    checkWhetherToReplenish(){
+        if (this.list.length < 7) {
+            this.list = [this.list,...this.getShuffledArray()]
+        }
+    }
 }
