c5fe2b6497e9e6e0ec7066dd11f942e4bb87244c
知念英弥 <s19013@std.it-college.ac.jp>
2024-01-15 17:24:40 +0900

    Fieldをクラス化

---
+++ src/Field.js
@@ -0,0 +1,121 @@
+import Block from "./Block"
+import {fieldWidth,fieldHeight} from "./Config"
+import lodash from 'lodash';
+export default class Field {
+    constructor(status = []) {
+        /** フィールド自体の状態 */
+        this.status = status
+        /** 各列の状態 埋まっているブロックの数 */
+        this.rowStatus = []
+
+        this.initialize()
+    }
+
+    initialize(){
+        this.updateRowStatus() // これで一番最初は全部0になるから全部0で埋めるコードがいらなくなる
+    }
+
+    /** 横1列を生成 */
+    generateRow(){
+        let row = []
+        for (let n = 0; n < fieldWidth; n++) {
+            row.push(new Block())
+        }
+        return row
+    }
+
+    generateField(){
+        // 沢山使うからキャッシュを作っとく?
+        let row = this.generateRow()
+        /** 縦を生成 */
+        for (let i = 0; i < fieldHeight; i++) {
+            this.status.push(lodash.cloneDeep(row))
+        }
+    }
+
+    /** 各列が何マス埋まっているのか確認する */
+    updateRowStatus(){
+        for (let index = 0; index < fieldHeight; index++) {
+            /** 配列の中身を全部足す */
+            this.rowStatus[index] = this.status[index].reduce((a,b) => {
+                return a + Number(b.isFill);
+            },0);
+        }
+    }
+    
+
+    /** 列を削除する */
+    vanishAlignedRows(){
+        // 4列以上揃うのはありえないのでループする最大の数を設定
+        for (let index = 0; index < 4; index++) {
+
+            // 全部埋まっている行を探す
+            let FoundedIndex = this.rowStatus.indexOf(fieldWidth)
+
+            // 見つからなかったら -1が帰ってくるのでそれ以外の値なら良い
+            if (FoundedIndex > -1) {
+                /** 揃った列自体を消してしまう */
+                this.status.splice(FoundedIndex, 1);
+
+                /** 消した分だけ上に加える */
+                this.status.unshift(this.generateRow())
+
+                // rowStatusの情報が古いままだと同じindex番号を削除し続けてしまうから
+                // 列を削除したら都度更新する必要がある
+                this.updateRowStatus()
+            } 
+            else { break } // 見つからないなら抜ける
+        }
+    }
+
+    // ---
+    // 描写関係
+    displayTetrimino(tetrimino){
+        for (let block of tetrimino.Coordinate) {
+            this.status[block.y][block.x].isFill = true
+            this.status[block.y][block.x].isMoving = true
+        }
+    }
+
+    undisplayTetrimino(tetrimino){
+        for (let block of tetrimino.Coordinate) {
+            this.status[block.y][block.x].isFill = false
+            this.status[block.y][block.x].isMoving = false
+        }
+    }
+
+
+    displayGhost(ghost){
+        for (let block of ghost.Coordinate) {
+            this.status[block.y][block.x].ghost = true
+        }
+    }
+
+    undisplayGhost(ghost){
+        for (let block of ghost.Coordinate) {
+            this.status[block.y][block.x].ghost = false
+        }
+    }
+
+    // 揃っている列に色を塗る
+    EnableLined(){
+        for (let index = 0; index < fieldHeight; index++) {
+            // 揃っていたらlinedプロパティをtrueにする
+            if (this.rowStatus[index] == fieldWidth) { 
+                for (const block of this.status[index]) { block.lined = true } 
+            }
+        }
+    }
+
+    /** 動かしているテトリミノを固定化する */
+    immobilization(){
+        // movingを外して固定化(動かせなく)する
+        for (let block of this.tetrimino.Coordinate) {
+            this.status[block.y][block.x].isMoving = false
+        }
+    }
+}
+
+// baseLineが便利な時
+// 一番最初のフィールド生成する時ぐらいか?
+// わざわざreturn new Field(this.status)しなくてもいいよね
