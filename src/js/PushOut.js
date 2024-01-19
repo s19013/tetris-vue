ffd74b12f1f3b9f0cadda12e83d579a144192aa5
知念英弥 <s19013@std.it-college.ac.jp>
2024-01-19 16:53:22 +0900

    メモに基づき修正どころか改造

--- src/js/PushOut.js
+++ src/js/PushOut.js
@@ -1,62 +1,100 @@
 import {fieldWidth,fieldHeight} from "./Config"
+import lodash from 'lodash';
 /**
  * 主に壁などからブロックを押し出す処理をする
  * 完全に出てくるまで繰り返すためwhile
  */
 
 export default class PushOut {
-    FromTheFloor(tetriminoCoordinate){
+    FromTheFloor(tetrimino){
         const bottom  = fieldHeight - 1;
-
-        for (let tentativeBlock of tetriminoCoordinate) {
-            while (tentativeBlock.y > bottom) {
-                tetriminoCoordinate.forEach(block => { block.y -= 1 });
-            }
+        for (let block of tetrimino.Coordinate) {
+            // 床から出てくるまで繰り返す
+            while (block.y > bottom) { tetrimino.moveUp() }
         }
 
-        return tetriminoCoordinate
+        return tetrimino
     }
 
-    FromTheRoof(tetriminoCoordinate){
-        for (let tentativeBlock of tetriminoCoordinate) {
-            while (tentativeBlock.y < 0) {
-                tetriminoCoordinate.forEach(block => { block.y += 1 });
-            }
+    FromTheRoof(tetrimino){
+        for (let block of tetrimino.Coordinate) {
+            while (block.y < 0) { tetrimino.moveDown() }
         }
 
-        return tetriminoCoordinate
+        return tetrimino
     }
 
-    FromTheLeftWall(tetriminoCoordinate){
-        let moved = false
-        for (let tentativeBlock of tetriminoCoordinate) {
-            while (tentativeBlock.x < 0) {
-                tetriminoCoordinate.forEach(block => { block.x += 1 });
-                moved = true
-            }
+    FromTheLeftWall(tetrimino){
+        for (let block of tetrimino.Coordinate) {
+            // 左壁から出てくるまで繰り返す
+            while (block.x < 0) { tetrimino.moveRight() }
         }
 
-        return {
-            tetriminoCoordinate:tetriminoCoordinate,
-            moved:moved
-        }
+        return tetrimino
     }
 
-    FromTheRightWall(tetriminoCoordinate){
+    FromTheRightWall(tetrimino){
         const rightEdge   = fieldWidth - 1
-        let moved = false
         
-        for (let tentativeBlock of tetriminoCoordinate) {
-            while (tentativeBlock.x > rightEdge) {
-                tetriminoCoordinate.forEach(block => { block.x -= 1 });
-                moved = true
-            }
+        for (let block of tetrimino.Coordinate) {
+            // 右壁から出てくるまで繰り返す
+            while (block.x > rightEdge) { tetrimino.moveLeft() }
         }
 
-        return {
-            tetriminoCoordinate:tetriminoCoordinate,
-            moved:moved
+        return tetrimino
+    }
+
+    /** 補正が必要かどうか調べる */
+    // 一つでも引っかるならfalse
+    isNeedCorrection(tetrimino){
+        if (this.isNeedFloorCorrection(tetrimino)) {return true}
+        if (this.isNeedLeftCorrection(tetrimino) ) {return true}
+        if (this.isNeedRightCorrection(tetrimino)) {return true}
+
+        return false
+    }
+
+    // 左壁を突き抜けてないか
+    isNeedLeftCorrection(tetrimino){
+        for (let block of tetrimino.Coordinate) {
+            if (block.x < 0) { return true }
         }
+        return false
+    }
+
+    // 右壁を突き抜けてないか
+    isNeedRightCorrection(tetrimino){
+        const rightEdge   = fieldWidth - 1
+        for (let block of tetrimino.Coordinate) {
+            if (block.x > rightEdge) { return true }
+        }
+        return false
+    }
+
+    // 床を突き抜けてないか
+    isNeedFloorCorrection(tetrimino){
+        const bottom  = fieldHeight - 1;
+        for (let block of tetrimino.Coordinate) {
+            if (block.y > bottom) { return true }
+        }
+        return false
+    }
+
+    /** 補正をかける*/
+    correction(tetrimino){
+        // 補正をかけた後(色々いじるからクローン)
+        let correctioned = lodash.cloneDeep(tetrimino)
+        if (this.isNeedFloorCorrection(correctioned)) {
+            correctioned = this.FromTheFloor(tetrimino)
+        }
+        if (this.isNeedLeftCorrection(correctioned)) {
+            correctioned = this.FromTheLeftWall(correctioned)
+        }
+        if (this.isNeedRightCorrection(correctioned)) {
+            correctioned = this.FromTheRightWall(correctioned)
+        }
+
+        return correctioned
     }
 
 }
