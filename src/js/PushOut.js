fec1eff30a79b2e9243300cfcc457a3cd7e6a32c
知念英弥 <s19013@std.it-college.ac.jp>
2024-01-19 20:56:13 +0900

    やろうとしたことが仕様上できないことが後からわかったため大半を元にもどした｡ いらない関数があったので削除

--- src/js/PushOut.js
+++ src/js/PushOut.js
@@ -6,95 +6,90 @@ import lodash from 'lodash';
  */
 
 export default class PushOut {
-    FromTheFloor(tetrimino){
+    FromTheFloor(coordinate){
         const bottom  = fieldHeight - 1;
-        for (let block of tetrimino.Coordinate) {
+        let correctedCoordinate = coordinate
+        for (let block of correctedCoordinate) {
             // 床から出てくるまで繰り返す
-            while (block.y > bottom) { tetrimino.moveUp() }
+            while (block.y > bottom) { coordinate.forEach(block => { block.y -= 1 }); }
         }
 
-        return tetrimino
+        return correctedCoordinate
     }
 
-    FromTheRoof(tetrimino){
-        for (let block of tetrimino.Coordinate) {
-            while (block.y < 0) { tetrimino.moveDown() }
-        }
-
-        return tetrimino
-    }
-
-    FromTheLeftWall(tetrimino){
-        for (let block of tetrimino.Coordinate) {
+    FromTheLeftWall(coordinate){
+        let correctedCoordinate = coordinate
+        for (let block of correctedCoordinate) {
             // 左壁から出てくるまで繰り返す
-            while (block.x < 0) { tetrimino.moveRight() }
+            while (block.x < 0) { correctedCoordinate.forEach(block => { block.x += 1 }); }
         }
 
-        return tetrimino
+        return correctedCoordinate
     }
 
-    FromTheRightWall(tetrimino){
+    FromTheRightWall(coordinate){
         const rightEdge   = fieldWidth - 1
+        let correctedCoordinate = coordinate
         
-        for (let block of tetrimino.Coordinate) {
+        for (let block of correctedCoordinate) {
             // 右壁から出てくるまで繰り返す
-            while (block.x > rightEdge) { tetrimino.moveLeft() }
+            while (block.x > rightEdge) { correctedCoordinate.forEach(block => { block.x -= 1 }); }
         }
 
-        return tetrimino
+        return correctedCoordinate
     }
 
     /** 補正が必要かどうか調べる */
     // 一つでも引っかるならfalse
-    isNeedCorrection(tetrimino){
-        if (this.isNeedFloorCorrection(tetrimino)) {return true}
-        if (this.isNeedLeftCorrection(tetrimino) ) {return true}
-        if (this.isNeedRightCorrection(tetrimino)) {return true}
+    isNeedCorrection(coordinate){
+        if (this.isNeedFloorCorrection(coordinate)) {return true}
+        if (this.isNeedLeftCorrection(coordinate) ) {return true}
+        if (this.isNeedRightCorrection(coordinate)) {return true}
 
         return false
     }
 
     // 左壁を突き抜けてないか
-    isNeedLeftCorrection(tetrimino){
-        for (let block of tetrimino.Coordinate) {
+    isNeedLeftCorrection(coordinate){
+        for (let block of coordinate) {
             if (block.x < 0) { return true }
         }
         return false
     }
 
     // 右壁を突き抜けてないか
-    isNeedRightCorrection(tetrimino){
+    isNeedRightCorrection(coordinate){
         const rightEdge   = fieldWidth - 1
-        for (let block of tetrimino.Coordinate) {
+        for (let block of coordinate) {
             if (block.x > rightEdge) { return true }
         }
         return false
     }
 
     // 床を突き抜けてないか
-    isNeedFloorCorrection(tetrimino){
+    isNeedFloorCorrection(coordinate){
         const bottom  = fieldHeight - 1;
-        for (let block of tetrimino.Coordinate) {
+        for (let block of coordinate) {
             if (block.y > bottom) { return true }
         }
         return false
     }
 
     /** 補正をかける*/
-    correction(tetrimino){
+    correction(coordinate){
         // 補正をかけた後(色々いじるからクローン)
-        let correctioned = lodash.cloneDeep(tetrimino)
-        if (this.isNeedFloorCorrection(correctioned)) {
-            correctioned = this.FromTheFloor(tetrimino)
+        let corrected = lodash.cloneDeep(coordinate)
+        if (this.isNeedFloorCorrection(corrected)) {
+            corrected = this.FromTheFloor(corrected)
         }
-        if (this.isNeedLeftCorrection(correctioned)) {
-            correctioned = this.FromTheLeftWall(correctioned)
+        if (this.isNeedLeftCorrection(corrected)) {
+            corrected = this.FromTheLeftWall(corrected)
         }
-        if (this.isNeedRightCorrection(correctioned)) {
-            correctioned = this.FromTheRightWall(correctioned)
+        if (this.isNeedRightCorrection(corrected)) {
+            corrected = this.FromTheRightWall(corrected)
         }
 
-        return correctioned
+        return corrected
     }
 
 }
