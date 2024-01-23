bc76019a76c0fa2f20107d736dc477fc43c5c9de
知念英弥 <s19013@std.it-college.ac.jp>
2024-01-23 22:34:42 +0900

    moveOO内で補正をかけずに外でかければまとめられるのでそうした

--- src/js/RotateHelper.js
+++ src/js/RotateHelper.js
@@ -7,38 +7,41 @@ export default class RotateHelper{
                 return {x:block.x,y:block.y -= 1 }
             }
         );
-        return pushOut.correction(moved);
+        return moved
     }
 
     moveDown(coordinate){
         const moved = coordinate.map(block => {
             return {x:block.x,y:block.y += 1}
         });
-        return pushOut.correction(moved)
+        return moved
     }
 
     moveLeft(coordinate){
         const moved = coordinate.map(block => {
             return {x:block.x -= 1,y:block.y}
         });
-        return pushOut.correction(moved)
+        return moved
     }
 
     moveRight(coordinate){
         const moved = coordinate.map(block => {
             return {x:block.x += 1,y:block.y}
         });
-        return pushOut.correction(moved)
+        return moved
     }
 
     // 回転入れ
     turnIn({field, coordinate, directions}) {
         // directionには関数名が入っていてそのまま関数を使えるらしい｡
         // jsならではの方法?
-        let moved = lodash.cloneDeep(coordinate)
+
+        let returnValue = lodash.cloneDeep(coordinate)
         for (const helper of directions) {
-          moved = helper(moved);
-          if (field.tetriminoIsNotOverlap(moved)) { return moved; }
+          const moved = helper(returnValue)
+          const corrected = pushOut.correction(moved)
+          returnValue = corrected
+          if (field.tetriminoIsNotOverlap(returnValue)) { return returnValue; }
         }
         // すべての方向で移動できなかった場合、失敗フラグを返す
         // 一般的にnullを返すのは良くないらしいが､失敗フラグとしてnullを使うのは良いらしい｡
@@ -48,10 +51,12 @@ export default class RotateHelper{
     // 上に上げる
     liftUp({field,coordinate}) {
         // 最大2回まで上げる
-        let moved = lodash.cloneDeep(coordinate)
+        let returnValue = lodash.cloneDeep(coordinate)
         for (let index = 0; index < 2; index++) {
-            moved = this.moveUp(moved)
-            if (field.tetriminoIsNotOverlap(moved)) { return moved; }
+            const moved = this.moveUp(returnValue)
+            const corrected = pushOut.correction(moved);
+            returnValue = corrected
+            if (field.tetriminoIsNotOverlap(returnValue)) { return returnValue; }
         }
         // すべての方向で移動できなかった場合、失敗フラグを返す
         // 一般的にnullを返すのは良くないらしいが､失敗フラグとしてnullを使うのは良いらしい｡
