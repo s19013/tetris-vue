919af9b80a193070b82f3c62d272269758551854
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-05 12:38:58 +0900

    b2bやtスピンでもスコア計算できるようにした｡(未テスト､未連携)

--- src/js/Score.js
+++ src/js/Score.js
@@ -13,6 +13,15 @@ export default class Score{
 
         /** 連続4列消し */
         this.back2back = false
+
+        // 通常消しの得点
+        this.points = [0,100,300,500,800]
+
+        // Tスピンの得点
+        this.tspinPoints = [400,800,1200,1600]
+
+        // パーフェクトクリア得点
+        this.PerfectClearPoints = [800,1000,1800,2000]
     }
 
     /** スコアを加える
@@ -26,34 +35,62 @@ export default class Score{
          else { console.error('Invalid score addition: ', ScoreToAdd); }
     }
 
-    /** 計算 */
-    calculation({countOfAlignedRows,level}){
+    scoreCalculater({base,level}){
+        // (基本スコア+RENスコア)*レベル*BackToBack+追加スコア
+        // b2b成立時1.5倍 不成立時1倍
 
-        /** 一列も揃ってない時はさっさと返す */
-        if (countOfAlignedRows == 0) {
-            this.back2back = false
-            this.ren = 0
-            return 
-        }
+        // 追加スコアはまだ未実装
 
-        this.addScore(countOfAlignedRows * countOfAlignedRows * level * 10)
+        let b2bCorrection = 1
+        if (this.back2back) { b2bCorrection = 1.5 }
 
-        if (countOfAlignedRows == 4) {
-            this.isTetris = true
+        this.addScore((base+this.ren)*level*b2bCorrection)
+    }
 
-            // これは表示用なので数秒たったら消す
-            setTimeout(()=> {
-                this.isTetris = false
-            },2000) ;
-        }
+    // Tスピン用
+    TspinCalculation({countOfAlignedRows,level}){
+        if (countOfAlignedRows == 0) { this.disableFlags() }
 
-        // b2b
-        // this.addScore(50 * level)
-
-        // ren
-        this.addScore(this.ren * 10 * level)
-        
         // renを加える
         this.ren += countOfAlignedRows
+
+        this.scoreCalculater({
+            base:this.tspinPoints[countOfAlignedRows],
+            level:level
+        })
+
+        if (countOfAlignedRows > 0) { this.back2back = true }
+    }
+
+    // 他のミノ用
+    calculation({countOfAlignedRows,level}){
+        if (countOfAlignedRows == 0) { this.disableFlags() }
+
+        // renを加える
+        this.ren += countOfAlignedRows
+
+        this.scoreCalculater({
+            base:this.points[countOfAlignedRows],
+            level:level
+        })
+
+        // b2b発動は消した後
+        if (countOfAlignedRows == 4) {
+            this.back2back = true
+            this.enableIsTetrisFlag()
+        }
+    }
+
+    disableFlags(){
+        this.back2back = false
+        this.ren = 0
+    }
+
+    enableIsTetrisFlag(){
+        this.isTetris = true
+        // これは表示用なので数秒たったら消す
+        setTimeout(()=> {
+            this.isTetris = false
+        },2000) ;
     }
 }
