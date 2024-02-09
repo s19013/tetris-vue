47322372722bd73ee06d3df9f9d1571d79ab4fda
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-09 23:40:46 +0900

    一定時間だけメッセージを表示するコンポーネントを作成

---
+++ src/components/FlashMessage.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      showFlag: false,
+      timerid: null
+    }
+  },
+  props: {
+    timeLimit: {
+      type: Number,
+      default: 2000
+    }
+  },
+  methods: {
+    showMessage() {
+      this.showFlag = true
+      // 数秒たったら消す
+      this.timerid = setTimeout(() => {
+        this.showFlag = false
+      }, this.timeLimit)
+    }
+  },
+  watch: {}
+}
+</script>
+
+<template>
+  <div class="flashMessage">
+    <p v-show="this.showFlag">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
